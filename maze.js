32c8f3a8c09b645e0aa1f1adbac516692ea11b53
Jonathan <rabadunk@gmail.com>
2019-10-21 23:57:08 +1300

    added cell object making it easier to change colors and what not

--- maze.js
+++ maze.js
@@ -1,39 +1,75 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext('2d');
 
-function draw() {
+var maze = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 1, 0, 1, 1, 1, 1, 1],
+    [1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+    [1, 2, 0, 1, 1, 1, 1, 1, 0, 1],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 1, 1, 0, 1, 0, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+];
 
-    var canvas = document.getElementById("canvas");
-    var ctx = canvas.getContext('2d');
-    
-    var maze = [
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 0, 1, 1, 0, 1, 1, 1, 1, 1],
-        [1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 1, 1, 1, 1, 0, 1],
-        [1, 1, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 1, 1, 0, 1, 0, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-    ]
-    
-    
-    
+var cellArray = [];
+
+var mazeFeature = {
+    EMPTY: 0,
+    WALL: 1,
+    INTERSECTION: 2
+}
+
+class Cell {
+    constructor(row, col, color) {
+        this.row = row;
+        this.col = col;
+        this.color = color;
+
+    }
+
+    show() {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.row*50, this.col*50, 50, 50);
+    }
+}
+
+function makeMaze() {
+
     for (var y = 0; y < maze.length; y++) {
-        for (var x = 0; x < maze.length; x++) {
+        for (var x = 0; x < maze[0].length; x++) {
+
     
             if(maze[y][x] == 1)
             {
-                ctx.fillStyle="red";
-                ctx.fillRect(x*50, y*50, 50, 50);
+                var cell = new Cell(x, y, "red");
             }
             else if(maze[y][x] == 0)
             {
-                ctx.fillStyle="grey";
-                ctx.fillRect(x*50, y*50, 50, 50);
+                var cell = new Cell(x, y, "snow");
             }
+            else if(maze[y][x] == 2)
+            {
+                var cell = new Cell(x, y, "grey");
+            }
+
+            cellArray.push(cell);
     
         }
     
     }
 
 }
+
+
+
+function draw() {
+
+    for(var i = 0; i < cellArray.length; i++) {
+        cellArray[i].show();
+    }
+
+}
+
+makeMaze();
 
 draw();
